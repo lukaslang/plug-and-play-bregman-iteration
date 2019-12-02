c686a44321dbd68988d0f3d419333a48d960e955
Lukas Lang <ll542@cam.ac.uk>
2019-12-02 20:32:18 +0100

    Added derivative operator.

---
+++ pnpbi/test/test_derivatives.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019 Lukas Lang
+#
+# This file is part of PNPBI.
+#
+#    PNPBI is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    PNPBI is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with PNPBI. If not, see <http://www.gnu.org/licenses/>.
+import numpy as np
+from pnpbi.util import derivatives
+import unittest
+
+
+class TestDerivatives(unittest.TestCase):
+
+    def test_deriv2dfw(self):
+        m, n = 4, 3
+        hx, hy = 2, 2
+
+        Lx = np.array([[-1, 1, 0, 0],
+                       [0, -1, 1, 0],
+                       [0, 0, -1, 1],
+                       [0, 0, 0, 0]]) / hx
+
+        Ly = np.array([[-1, 1, 0],
+                       [0, -1, 1],
+                       [0, 0, 0]]) / hy
+
+        Dx, Dy = derivatives.deriv2dfw(m, n, hx, hy)
+        np.testing.assert_allclose(Dx.toarray(), Lx)
+        np.testing.assert_allclose(Dy.toarray(), Ly)
+
+    def test_deriv2dfw_one_by_one(self):
+        m, n = 1, 1
+        hx, hy = 1, 1
+
+        Lx = 0 / hx
+        Ly = 0 / hy
+
+        Dx, Dy = derivatives.deriv2dfw(m, n, hx, hy)
+        np.testing.assert_allclose(Dx.toarray(), Lx)
+        np.testing.assert_allclose(Dy.toarray(), Ly)
+
+    def test_deriv2dfw_apply_to_ones(self):
+        m, n = 5, 4
+        hx, hy = 2, 2
+
+        # Create derivative operators.
+        Dx, Dy = derivatives.deriv2dfw(m, n, hx, hy)
+
+        # Apply to constant matrix.
+        f = np.ones((n, m))
+        fx = f * Dx.transpose()
+        fy = Dy * f
+
+        np.testing.assert_allclose(fx, np.zeros((n, m)))
+        np.testing.assert_allclose(fy, np.zeros((n, m)))
+
+    def test_deriv2dfw_adjoint(self):
+        m, n = 4, 5
+        hx, hy = 2, 2
+
+        # Create derivative operators.
+        Dx, Dy = derivatives.deriv2dfw(m, n, hx, hy)
+
+        # Create random matrix.
+        f = np.random.rand(n, m)
+
+        # Create second random matrix.
+        g = np.random.rand(n, m)
+
+        # Apply to constant matrix.
+        fx = f * Dx.transpose()
+        fy = Dy * f
+
+        # Compute divergence.
+        gx = g * Dx
+        gy = Dy.transpose() * g
+
+        np.testing.assert_allclose(np.dot(fx.flatten(), g.flatten()),
+                                   np.dot(f.flatten(), gx.flatten()))
+        np.testing.assert_allclose(np.dot(fy.flatten(), g.flatten()),
+                                   np.dot(f.flatten(), gy.flatten()))
+
+    def test_vecderiv2dfw(self):
+        m, n = 4, 3
+        hx, hy = 2, 2
+
+        Lx = np.array([[-1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, -1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, -1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, -1, 0, 0, 1, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, -1, 0, 0, 1, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, -1, 0, 0, 1, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, -1, 0, 0, 1, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 1, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 1],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]) / hx
+
+        Ly = np.array([[-1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, -1, 1, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, -1, 1, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, -1, 1, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, -1, 1, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 1],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]) / hy
+
+        Dx, Dy = derivatives.vecderiv2dfw(m, n, hx, hy)
+        np.testing.assert_allclose(Dx.toarray(), Lx)
+        np.testing.assert_allclose(Dy.toarray(), Ly)
+
+    def test_vecderiv2dfw_adjoint(self):
+        m, n = 4, 5
+        hx, hy = 2, 2
+
+        # Create derivative operators.
+        Dx, Dy = derivatives.vecderiv2dfw(m, n, hx, hy)
+
+        # Create random matrix.
+        f = np.random.rand(n * m)
+
+        # Create second random matrix.
+        g = np.random.rand(n * m)
+
+        # Apply to constant matrix.
+        fx = Dx * f
+        fy = Dy * f
+
+        # Compute divergence.
+        gx = Dx.transpose() * g
+        gy = Dy.transpose() * g
+
+        np.testing.assert_allclose(np.dot(fx.flatten(), g.flatten()),
+                                   np.dot(f.flatten(), gx.flatten()))
+        np.testing.assert_allclose(np.dot(fy.flatten(), g.flatten()),
+                                   np.dot(f.flatten(), gy.flatten()))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ pnpbi/util/derivatives.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2019 Lukas Lang
+#
+# This file is part of PNPBI.
+#
+#    PNPBI is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    PNPBI is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with PNPBI. If not, see <http://www.gnu.org/licenses/>.
+import numpy as np
+import scipy.sparse as sparse
+
+
+def deriv2dfw(m: int, n: int, hx: float, hy: float) -> (np.array, np.array):
+    """Takes the number of columns m, the number of rows n, and spatial scaling
+    parameters hx and hy, and creates first order forward difference matrices
+    Dx and Dy with Neumann boundary conditions.
+
+    The gradient of a matrix f of size [n, m] is then given by
+
+        grad(f) = np.stack((f * Dx.transpose(), Dy * f), axis=2)
+
+    For v = (v1, v2), the adjoint -div(v) is then given by
+
+        -div(v) = v1 * Dx + Dy.transpose() * v2.
+
+    Args:
+        m (int): Number of columns. m > 1.
+        n (int): Number of rows. n > 1.
+        hx (float): Spatial scaling parameter in column axis.
+        hy (float): Spatial scaling parameter in row axis.
+
+    Returns:
+        Dx (np.array): Array of shape (m, m).
+        Dy (np.array): Array of shape (n, n).
+    """
+    v1 = -np.ones((1, m)) / hx
+    v1[0, m - 1] = 0
+    v2 = np.ones((1, m)) / hx
+    Dx = sparse.spdiags(np.vstack((v1, v2)), [0, 1], m, m)
+
+    v1 = -np.ones((1, n)) / hy
+    v1[0, n - 1] = 0
+    v2 = np.ones((1, n)) / hy
+    Dy = sparse.spdiags(np.vstack((v1, v2)), [0, 1], n, n)
+    return Dx, Dy
+
+
+def vecderiv2dfw(m: int, n: int, hx: float, hy: float) -> (np.array, np.array):
+    """Takes the number of columns m, the number of rows n, and spatial scaling
+    parameters hx and hy, and creates first order forward difference matrices
+    Dx and Dy with Neumann boundary conditions.
+
+    The gradient of a matrix f in vector form, i.e. shape (n*m, 1) is then
+    given by
+
+        grad(f) = np.stack((Dx * f, Dy * f), axis=1)
+
+    For v = (v1, v2), the adjoint -div(v) is then given by
+
+        -div(v) = Dx.transpose() * v1 + Dy.transpose() * v2.
+
+    Args:
+        m (int): Number of columns. m > 1.
+        n (int): Number of rows. n > 1.
+        hx (float): Spatial scaling parameter in column axis.
+        hy (float): Spatial scaling parameter in row axis.
+
+    Returns:
+        Dx (np.array): Array of shape (n*m, n*m).
+        Dy (np.array): Array of shape (n*m, n*m).
+    """
+    Dx, Dy = deriv2dfw(m, n, hx, hy)
+    Dx = sparse.kron(Dx, sparse.identity(n))
+    Dy = sparse.kron(sparse.identity(m), Dy)
+    return Dx, Dy
