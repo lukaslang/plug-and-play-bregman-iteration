Fixed problem with gradient.

pnpbi/util/radon.py
@@ -44,12 +44,12 @@ class RadonTransform:
         def Rfun(x: torch.Tensor) -> torch.Tensor:
             x_np = x.detach().cpu().numpy()
             y_np = R(x_np)
-            return torch.from_numpy(y_np)
+            return x.new(y_np)
 
         def Radjfun(y: torch.Tensor) -> torch.Tensor:
             y_np = y.numpy()
             x_np = Radj(y_np)
-            return torch.from_numpy(x_np)
+            return y.new(x_np)
 
         self.R = Rfun
         self.Radj = Radjfun